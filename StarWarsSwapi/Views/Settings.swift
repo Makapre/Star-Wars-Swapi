a5f7cdf822c08a5bd75a7cbad7811f39ddeca9df
Marius Preikschat <m.preikschat@googlemail.com>
2024-10-02 09:10:31 +0200

    adds mac catalyst platform

--- StarWarsSwapi/Views/Settings.swift
+++ StarWarsSwapi/Views/Settings.swift
@@ -14,63 +14,71 @@ struct Settings: View {
     @AppStorage("currency") private var currency = "Credits"
 
     @Environment(\.openURL) var openURL
+    @Environment(\.dismiss) private var dismiss
 
     private var segmentCurrency = ["Credits", "Euro"]
 
     var body: some View {
-        List {
-            Section("Comparrison object") {
-                Text("Which object to compare with?")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Picker("Which object to compare with?", selection: $segmentSelectionComparisson) {
-                    ForEach(ComparisonPlanets.allCases, id: \.self) {
-                        Text($0.rawValue)
+        NavigationStack {
+            List {
+                Section("Comparrison object") {
+                    Text("Which object to compare with?")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Picker("Which object to compare with?", selection: $segmentSelectionComparisson) {
+                        ForEach(ComparisonPlanets.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Currency") {
+                    Text("Which currency to use?")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Picker("Which currency to use?", selection: $segmentSelectionCurrency) {
+                        ForEach(segmentCurrency, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Info") {
+                    Text("Developed as a Coding challenge by Marius Preikschat")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("Api used [swapi.dev](https://swapi.dev/documentation#root)")
+                        .tint(.blue)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+
+                if let url = URL(string: "https://github.com/Makapre/Star-Wars-Swapi") {
+                    Button {
+                        openURL(url)
+                    } label: {
+                        Label("Github repository", systemImage: "square.and.arrow.up")
+                            .foregroundStyle(.link)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+            }
+            .onAppear {
+                segmentSelectionComparisson = comparisonPlanet
+                segmentSelectionCurrency = currency
+            }
+            .onDisappear {
+                comparisonPlanet = segmentSelectionComparisson
+                currency = segmentSelectionCurrency
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
                     }
                 }
-                .padding()
-                .pickerStyle(.segmented)
             }
-            .padding(.top)
-
-            Section("Currency") {
-                Text("Which currency to use?")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Picker("Which currency to use?", selection: $segmentSelectionCurrency) {
-                    ForEach(segmentCurrency, id: \.self) {
-                        Text($0)
-                    }
-                }
-                .padding()
-                .pickerStyle(.segmented)
-                .presentationDetents([.large])
-                .presentationDragIndicator(.visible)
-            }
-
-            Section("Info") {
-                Text("Developed as a Coding challenge by Marius Preikschat")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text("Api used [swapi.dev](https://swapi.dev/documentation#root)")
-                    .tint(.blue)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-
-            if let url = URL(string: "https://github.com/Makapre/Star-Wars-Swapi") {
-                Button {
-                    openURL(url)
-                } label: {
-                    Label("Github repository", systemImage: "square.and.arrow.up")
-                        .foregroundStyle(.link)
-                }
-                .frame(maxWidth: .infinity, alignment: .center)
-            }
-        }
-        .onAppear {
-            segmentSelectionComparisson = comparisonPlanet
-            segmentSelectionCurrency = currency
-        }
-        .onDisappear {
-            comparisonPlanet = segmentSelectionComparisson
-            currency = segmentSelectionCurrency
+            .navigationTitle("Preferences")
+            .presentationDetents([.large])
+            .presentationDragIndicator(.visible)
         }
     }
 }
